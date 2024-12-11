9dfd4deac7e82b53be72f26c261c9d384fb5bdb3
WitchingHr <matt1494@gmail.com>
2024-12-11 18:49:28 -0500

    fix state delay, add typing

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
+
+// bookshelf component
+const Bookshelf = () => {
+  // collection state
+  const [collection, setCollection] = useState<string[]>([]);
+
+	return (
+		<div>
+			<div className="border m-2">
+        {/* pass down collection state to shelf for rendering */}
+				<Shelf collection={collection} />
+			</div>
+      {/* pass down collection state to form for adding books */}
+			<Form collection={collection} setCollection={setCollection}  />
+		</div>
+	);
+};
+
+// interface Book {
+//   name: string;
+// }
+
+interface FormProps {
+  collection: string[];
+  setCollection: Dispatch<SetStateAction<string[]>>;
+}
 
 // form for adding books to the shelf
-const MyForm = () => {
+const Form: React.FC<FormProps> = ({collection, setCollection}) => {
 	// state for input value
-	const [name, setName] = useState("");
-	// state for saving name
-	const [savedName, setSavedName] = useState("");
+	const [name, setName] = useState<string>("");
 
 	// update input value
 	function handleInputChange(event: any) {
@@ -15,7 +39,7 @@ const MyForm = () => {
 	// save input value as name
 	function handleSumbit(event: any) {
 		event?.preventDefault();
-		setSavedName(name);
+    setCollection([...collection, name]);
 		setName("");
 	}
 
@@ -32,21 +56,23 @@ const MyForm = () => {
 						className="border"
 					/>
 				</label>
-				<button type="submit">Add</button>
+				{name && <button type="submit">Add</button>}
 			</form>
-
-			{/* show name */}
-			{savedName && <p>{savedName}</p>}
 		</div>
 	);
 };
 
-// bookshelf component
-const Bookshelf = () => {
+interface ShelfProps {
+  collection: string[];
+}
+
+// shelf
+const Shelf: React.FC<ShelfProps> = ({collection}) => {
 	return (
-		<div>
-			<div className="flex border m-2">Books go here</div>
-			<MyForm />
+		<div className="flex border m-2 h-24">
+			{collection.map((book: string, id: number) => (
+				<div className="border bg-red-600" key={id}>{book}</div>
+			))}
 		</div>
 	);
 };
