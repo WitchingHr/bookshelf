8e179c173d49cc9eaded953ceb1e4e7ce2dea5b1
WitchingHr <matt1494@gmail.com>
2024-12-18 07:20:55 -0500

    fix modal typing

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,21 @@
-import type { BookProps } from "./Book";
 import { useModal } from "./ModalContext";
 
-const Modal: React.FC<BookProps> = ({ name, author, cover }) => {
+const Modal: React.FC = () => {
 	const { modalData, setModalData } = useModal();
 
+	// hide modal by default
 	if (!modalData) return null;
 
 	return (
-		<div onClick={() => setModalData(null)} className="flex justify-center items-center absolute z-20 h-full w-full bg-black/50">
+		// clicking outside of modal closes it
+		<div
+			onClick={() => setModalData(null)}
+			className="flex justify-center items-center absolute z-20 h-full w-full bg-black/50"
+		>
 			<div className="h-2/3 w-1/3 bg-white rounded-md">
 				<p>{modalData.name}</p>
 				<p>{modalData.author}</p>
-				{modalData.img && <p>{modalData.img}</p>}
+				{modalData.cover && <p>{modalData.cover}</p>}
 			</div>
 		</div>
 	);
--- src/components/ModalContext.tsx
+++ src/components/ModalContext.tsx
@@ -1,16 +1,36 @@
-import { createContext, useContext, useState } from "react";
+import {
+	createContext,
+	useContext,
+	useState,
+	type Dispatch,
+	type SetStateAction,
+} from "react";
 
 import { type BookProps } from "./Book";
 
-const ModalContext = createContext<BookProps | null>(null);
-
-export const useModal = () => useContext(ModalContext);
-
-interface Props {
-  children: React.ReactNode
+interface ModalContextProps {
+	modalData: BookProps | null;
+	setModalData: Dispatch<SetStateAction<BookProps | null>>;
 }
 
-const ModalProvider: React.FC<Props> = ({ children }) => {
+// create context for modal
+const ModalContext = createContext<ModalContextProps | null>(null);
+
+export const useModal = () => {
+	const context = useContext(ModalContext);
+	// handle null
+	if (!context) {
+		throw new Error("useModal must be used within a ModalProvider");
+	}
+	return context;
+};
+
+interface ModalProviderProps {
+	children: React.ReactNode;
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
+	//  state for passing data from books to modal
 	const [modalData, setModalData] = useState<BookProps | null>(null);
 
 	return (
